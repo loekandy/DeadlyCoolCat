bb4a276a9a51426f16f678bd98688bafb0afe610
loekandy <loekandy@gmail.com>
2018-11-03 16:51:37 +0100

    Cool updates

--- CoolCat/webpack.config.vendor.js
+++ CoolCat/webpack.config.vendor.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
-    const extractCSS = new ExtractTextPlugin('vendor.css');
+    //const extractCSS = new ExtractTextPlugin('vendor.css');
     const isDevBuild = !(env && env.prod);
     return [{
         stats: { modules: false },
@@ -12,8 +12,8 @@ module.exports = (env) => {
         },
         module: {
             rules: [
-                { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' },
-                { test: /\.css(\?|$)/, use: extractCSS.extract([ isDevBuild ? 'css-loader' : 'css-loader?minimize' ]) }
+                { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' }
+                //{ test: /\.css(\?|$)/, use: extractCSS.extract([ isDevBuild ? 'css-loader' : 'css-loader?minimize' ]) }
             ]
         },
         entry: {
@@ -26,7 +26,7 @@ module.exports = (env) => {
             library: '[name]_[hash]',
         },
         plugins: [
-            extractCSS,
+            //extractCSS,
             new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
             new webpack.DllPlugin({
                 path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
