83b9bdeaaabb2586e0a30be20b28504551296e4c
loekandy <loekandy@gmail.com>
2018-11-03 17:10:18 +0100

    Added sections in layouts

--- CoolCat/ClientApp/components/Layout.tsx
+++ CoolCat/ClientApp/components/Layout.tsx
@@ -9,9 +9,17 @@ export class Layout extends React.Component<LayoutProps, {}> {
     public render() {
         return (
             <div>
+                <div id="topNav">
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-static-top">
-                    <p className="display-4 text-light">Project Name</p>
-                </nav>
+                        <div>
+                            <div className="display-4 text-light">Coolcat</div>
+                            <div className="slogan">Deadly</div>
+                        </div>
+                    </nav>
+                    </div>
+                <div id="first-section"></div>
+                <div id="second-section"></div>
+                <div id="third-section"></div>
             </div>);
     }
 }
