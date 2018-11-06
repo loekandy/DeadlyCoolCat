26d4bdd004dbdbb4b9ae86a260170c976ddb2bac
Andy Johansson <loekandy@gmail.com>
2018-11-06 16:52:27 +0100

    Zombie CSS added

--- CoolCat/ClientApp/components/Intro.tsx
+++ CoolCat/ClientApp/components/Intro.tsx
@@ -3,6 +3,44 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default class Intro extends React.Component<{}, {}> {
     public render() {
-        return <div><div id="first-section"></div></div>;
+        return (<div>
+            <div className="zombie-container">
+                <div className="zombie">
+                    <div className="ear left">
+                    </div>
+                    <div className="head">
+                        <div className="upper-part">
+                            <div className="scars upper">
+                                <div className="scar"></div>
+                            </div>
+                            <div className="features">
+                                <div className="eyes">
+                                    <div className="eye left">
+                                        <div className="pupil">
+                                            <div className="iris"></div>
+                                        </div>
+                                    </div>
+                                    <div className="eye right">
+                                        <div className="pupil">
+                                            <div className="iris"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mouth">
+                                    <div className="tooth top right"></div>
+                                    <div className="tooth top left"></div>
+                                    <div className="tooth bottom center"></div>
+                                </div>
+                            </div>
+                            <div className="scars bottom">
+                                <div className="clip-scar"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ear right">
+                    </div>
+                </div>
+            </div>
+        </div>);
     }
 }
