8b363ef82d70b63b17d14674f9f422628fdbccb0
Andy Johansson <loekandy@gmail.com>
2018-11-15 08:13:44 +0100

    Update CoolCat/ClientApp/components/StarWars.tsx

--- CoolCat/ClientApp/components/StarWars.tsx
+++ CoolCat/ClientApp/components/StarWars.tsx
@@ -2,9 +2,13 @@
 import { Link, NavLink } from 'react-router-dom';
 
 export default class StarWars extends React.Component<{}, {}> {
+
+    //Använd map funktionen
+
     public render() {
         return (
             <div id="third-section">
+                
                 <button type="button" className="btn btn-dark button">Press Me</button>
             </div>
         );
